6fd7728b6054602ecccc96d48be8fa7213f65373
dreskev1 <nosequeponer1579@gmail.com>
2023-11-13 18:56:59 -0500

    add: cambios_direcciones

--- frontend/src/pages/direcciones.js
+++ frontend/src/pages/direcciones.js
@@ -57,7 +57,7 @@ function direcciones (){
 
              <br/>
              </div>
-
+             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.8667681442694!2d-75.6090559905333!3d6.281240225907186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e44294f9cdf1e11%3A0x770446289fd97374!2sCl.%2064%20%2396-1%2C%20Fuente%20Clara%2C%20Medell%C3%ADn%2C%20Robledo%2C%20Medell%C3%ADn%2C%20Antioquia!5e0!3m2!1ses!2sco!4v1699555571683!5m2!1ses!2sco" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             <Footer/>
        </div>
     );
